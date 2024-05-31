fd01f5520a9a4d639a64c6a49519d87e19b0240e
oscarhuang1117 <oscar860700891117@gmail.com>
2024-05-31 23:35:52 +0800

    use HW

--- train_cnn.py
+++ train_cnn.py
@@ -15,6 +15,7 @@ def main():
     numActions = 3
     obsSize = 12
     lr = 0.001
+    gamma = 0.99
     folder_name = "ex1"
     output_path = f"./result/{folder_name}"
     
@@ -23,21 +24,45 @@ def main():
     optimizer = torch.optim.Adam(
         policy.parameters(), lr=lr)
     os.makedirs(output_path, exist_ok=True)
-    
-    sum_reward = 0
+    rewards=[]  
+    log_pis=[]
+    # sum_reward = 0
 
     for _ in range(NUM_EPISODES):
-        obs = env.reset()
+        state = env.reset()
         done = False
-        i = 0
+        # i = 0
+        sum_reward = 0
         while not done:
-            plt.imshow(obs, interpolation='nearest')
+            plt.imshow(state, interpolation='nearest')
             plt.show()
-            action = policy.choose_action(obs)
-            obs, reward, done, info = env.step(action)
+            (action, log_pi) = policy.choose_action(state)
+            next_state, reward, done, info = env.step(action)
             
             sum_reward += reward
-            
+            rewards.append(reward)
+            log_pis.append(log_pi)
+
+            if done: break 
+            state = next_state
+        tot_return = 0
+        tot_returns=torch.zeros(len(rewards), dtype=torch.float32) # clean gradient
+
+        for i in reversed(range(len(rewards))): # reward to go
+            tot_return = rewards[i] + tot_return*gamma
+            tot_returns[i] = tot_return
+        # print(tot_returns)
+
+        for t in range(len(tot_returns)): # pseudocode
+            tot_returns[t] = (gamma ** t) * tot_returns[t]
+        # print(tot_returns)
+
+        logpi_stack=torch.stack(log_pis)
+        gradient=torch.dot(tot_returns, logpi_stack) # sum(times) = dot
+        gradient.backward()
+        # theta = theta + alpha * gradient
+        optimizer.step() # Adam not SGD
+        optimizer.zero_grad()
         # print(info["snake_length"])
         # print(info["food_pos"])
         # print(obs)
